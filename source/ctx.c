240bdb9619a25912db11f5aba6f6e2a479b044b1
etienne-hd <hode.etienne@gmail.com>
2026-05-25 23:19:42 +0200

    fix(ctx): zero initialize context before setting arguments

--- source/ctx.c
+++ source/ctx.c
@@ -6,17 +6,19 @@
 /*   By: ehode <ehode@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 23:11:31 by ehode             #+#    #+#             */
-/*   Updated: 2026/05/25 23:17:35 by ehode            ###   ########.fr       */
+/*   Updated: 2026/05/25 23:19:14 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ctx.h"
+#include "libft.h"
 #include <stdlib.h>
 
 t_ctx	init_ctx(int argc, char **argv)
 {
 	t_ctx	ctx;
 
+	ft_bzero(&ctx, sizeof(ctx));
 	ctx.arg_count = argc - 1;
 	ctx.args = argv + 1;
 	ctx.args_state = malloc(sizeof(char) * ctx.arg_count / 4 + (ctx.arg_count
