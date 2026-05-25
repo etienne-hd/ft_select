caba73c567b00b6ada1cee60599bae2de8080781
etienne-hd <hode.etienne@gmail.com>
2026-05-25 22:54:22 +0200

    feat(input): add key parser for ascii control and escape sequences

---
+++ include/key_parser.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_parser.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehode <ehode@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 21:30:07 by ehode             #+#    #+#             */
+/*   Updated: 2026/05/25 22:53:47 by ehode            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEY_PARSER_H
+# define KEY_PARSER_H
+# include <sys/types.h>
+
+typedef enum e_keymaps
+{
+	KEY_NONE = 0,
+	KEY_A,
+	KEY_B,
+	KEY_C,
+	KEY_D,
+	KEY_E,
+	KEY_F,
+	KEY_G,
+	KEY_H,
+	KEY_I,
+	KEY_J,
+	KEY_K,
+	KEY_L,
+	KEY_M,
+	KEY_N,
+	KEY_O,
+	KEY_P,
+	KEY_Q,
+	KEY_R,
+	KEY_S,
+	KEY_T,
+	KEY_U,
+	KEY_V,
+	KEY_W,
+	KEY_X,
+	KEY_Y,
+	KEY_Z,
+	KEY_ESCAPE,
+	KEY_ENTER,
+	KEY_TAB,
+	KEY_BACKSPACE,
+	KEY_DELETE,
+	KEY_ARROW_UP,
+	KEY_ARROW_DOWN,
+	KEY_ARROW_LEFT,
+	KEY_ARROW_RIGHT
+}	t_keymaps;
+
+typedef struct s_key
+{
+	t_keymaps	code;
+	char		ctrl_pressed;
+	char		shift_pressed;
+}	t_key;
+
+t_key	get_key(char *buffer, uint *cursor);
+
+#endif
---
+++ source/key_parser.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehode <ehode@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 21:29:58 by ehode             #+#    #+#             */
+/*   Updated: 2026/05/25 22:52:52 by ehode            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "key_parser.h"
+#include "libft.h"
+
+static t_key	parse_escape_sequence(char *buffer, uint *cursor)
+{
+	if (buffer[0] == 0)
+		return ((t_key){.code = KEY_ESCAPE, .ctrl_pressed = 0,
+			.shift_pressed = 0});
+	if (buffer[0] == '[')
+	{
+		(*cursor)++;
+		if (buffer[1])
+			(*cursor)++;
+		if (buffer[1] == 'A')
+			return ((t_key){.code = KEY_ARROW_UP, .ctrl_pressed = 0,
+				.shift_pressed = 0});
+		else if (buffer[1] == 'B')
+			return ((t_key){.code = KEY_ARROW_DOWN, .ctrl_pressed = 0,
+				.shift_pressed = 0});
+		else if (buffer[1] == 'C')
+			return ((t_key){.code = KEY_ARROW_RIGHT, .ctrl_pressed = 0,
+				.shift_pressed = 0});
+		else if (buffer[1] == 'D')
+			return ((t_key){.code = KEY_ARROW_LEFT, .ctrl_pressed = 0,
+				.shift_pressed = 0});
+	}
+	return ((t_key){.code = KEY_NONE, .ctrl_pressed = 0, .shift_pressed = 0});
+}
+
+static t_key	parse_control_char(char *buffer, uint *cursor)
+{
+	(*cursor)++;
+	if (buffer[0] == 8)
+		return ((t_key){.code = KEY_BACKSPACE, .ctrl_pressed = 0,
+			.shift_pressed = 0});
+	else if (buffer[0] == 9)
+		return ((t_key){.code = KEY_TAB, .ctrl_pressed = 0,
+			.shift_pressed = 0});
+	else if (buffer[0] == 13)
+		return ((t_key){.code = KEY_ENTER, .ctrl_pressed = 0,
+			.shift_pressed = 0});
+	else
+	{
+		return ((t_key){.code = KEY_A + buffer[0] - 1, .ctrl_pressed = 1,
+			.shift_pressed = 0});
+	}
+}
+
+static t_key	parse_ascii(char *buffer, uint *cursor)
+{
+	(*cursor)++;
+	if (buffer[0] >= 'a' && buffer[0] <= 'z')
+		return ((t_key){.code = KEY_A + buffer[0] - 'a', .shift_pressed = 0,
+			.ctrl_pressed = 0});
+	return ((t_key){.code = KEY_A + buffer[0] - 'A', .shift_pressed = 1,
+		.ctrl_pressed = 0});
+}
+
+t_key	get_key(char *buffer, uint *cursor)
+{
+	t_key	key;
+
+	(*cursor)++;
+	ft_bzero(&key, sizeof(t_key));
+	if (buffer[0] == 0)
+		return (key);
+	else if (buffer[0] == 27)
+		key = parse_escape_sequence(buffer + 1, cursor);
+	else if (buffer[0] < 32)
+		key = parse_control_char(buffer, cursor);
+	else if (buffer[0] == 127)
+		key.code = KEY_DELETE;
+	else
+		key = parse_ascii(buffer, cursor);
+	return (key);
+}
